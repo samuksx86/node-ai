ddcbf7ab4da0df6f66257589d76518f6df5aab65
Samuel <samuks2005@outlook.com>
2025-07-07 14:14:44 -0300

    feat: update application name and description, fix module import paths

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ThrottlerModule } from '@nestjs/throttler';
 
-import { SharedModule } from './_shared/infra/ioc/shared.module';
-import { AppController } from './app.controller';
-
+import { SharedModule } from '~_shared/infra/ioc/shared.module';
+import { AppController } from '~app.controller';
 import { ChatModule } from '~chat/infra/ioc/chat.module';
 import { MaterialsModule } from '~materials/infra/ioc/materials.module';
 
@@ -18,9 +17,10 @@ const applicationConfig = [
   }),
 ];
 
-const aplicationModules = [SharedModule, ChatModule, MaterialsModule];
+const applicationModules = [SharedModule, ChatModule, MaterialsModule];
+
 @Module({
-  imports: [...applicationConfig, ...aplicationModules],
+  imports: [...applicationConfig, ...applicationModules],
   controllers: [AppController],
 })
 export class AppModule {}
