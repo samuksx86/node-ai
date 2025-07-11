c45b87aa2b9691880f03f8094872fe81fd0a9f8a
Samuel <samuks2005@outlook.com>
2025-07-11 16:52:05 -0300

    feat: update App component structure and integrate Tailwind CSS

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,9 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -14,10 +14,11 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      'plugin:prettier/recommended',
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-])
+]);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,51 +1,5 @@
-import { useState } from 'react';
-import './App.css';
-import ChatHistory from './components/ChatHistory';
-import ChatInput from './components/ChatInput';
-import { createUserMessage, generateResponseMessage } from './services/messageService';
-import type { Message } from './types';
-
 function App() {
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [input, setInput] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (input.trim() === '' || isLoading) return;
-
-    const userMessage = createUserMessage(input);
-
-    setMessages(prev => [...prev, userMessage]);
-    setInput('');
-    setIsLoading(true);
-
-    setTimeout(async () => {
-      const assistantMessage = await generateResponseMessage(userMessage.content);
-
-      setMessages(prev => [...prev, assistantMessage]);
-      setIsLoading(false);
-    }, 1000);
-  };
-
-  return (
-    <div className="chat-container">
-      <h1 className="app-title">Bino</h1>
-
-      <ChatHistory 
-        messages={messages} 
-        isLoading={isLoading} 
-      />
-
-      <ChatInput 
-        input={input} 
-        setInput={setInput} 
-        handleSubmit={handleSubmit} 
-        isLoading={isLoading} 
-      />
-    </div>
-  );
+  return <div className={'bg-emerald-800'}>routes</div>;
 }
 
 export default App;
